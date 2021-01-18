b3419d70932751d97fd5d28b40fe585fa541f3b4
Zhavgarat <67911288+Zhavgarat@users.noreply.github.com>
2021-01-18 04:00:54 +0300

    Update identidock.py

--- app/identidock.py
+++ app/identidock.py
@@ -1,7 +1,9 @@
 from flask import Flask, Response, request
 import requests
 import hashlib
+import html
 import redis
+
 
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
@@ -14,7 +16,7 @@ def mainpage():
 
     name = default_name
     if request.method == 'POST':
-        name = request.form['name']
+        name = html.escape( request.form['name'], quote=True )
 
     salted_name = salt + name
     name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
@@ -33,11 +35,11 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
-
+    name = html.escape(name, quote=True) 
     image = cache.get(name)
     if image is None:
         print ("Cache miss", flush=True)
-        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=200')
+        r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
         image = r.content
         cache.set(name, image)
 
